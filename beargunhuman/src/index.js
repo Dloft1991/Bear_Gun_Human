e5054c9693acd0c03856c296abd52b78bf5bbfa5
Dloft <derek.lofton1@gmail.com>
2020-03-17 18:59:50 -0400

    playingWithReact

--- beargunhuman/src/index.js
+++ beargunhuman/src/index.js
@@ -1,28 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+// import Car from './App.js';
 
-const myelement = 
-<div>
-    <h1>Hello JSX</h1>
-    <p>JSX is awesome!</p>
-    <div class="row">
-        <div class="col-sm-6">
-            <p> 1 col</p>
-        </div>
-        <div class="col-sm-6">
-            <p> 2 col</p>
-        </div>
-    </div>
-</div>
-const element2 = (
-    <ul>
-    <li>Apples</li>
-    <li>Oranges</li>
-    <li>Bananas</li>
-</ul>
-);
+class Car extends React.Component {
+    render() {
+        return <h2>I am a {this.props.brand}!</h2>
+    }
+}
 
+const myelement = <Car brand="Dodge" />;
 ReactDOM.render(myelement, document.getElementById('root'));
-ReactDOM.render(element2, document.getElementById('trunk'));
 
